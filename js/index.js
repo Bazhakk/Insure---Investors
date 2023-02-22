ddb798c38e2d65c4d9f4fab108df0346d9ea54c9
unknown <bazhakk@gmail.com>
2023-02-22 21:09:33 +0200

    site tincture

---
+++ js/index.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function ( $ ) {
+  
+  $(document).on('click', '#burger', function() {
+    $('#burger').toggleClass('burger-active');
+    $('#mobile-menu').slideToggle().css('display', 'flex');
+  });
+
+  $(document).on('click', function(e) {
+    if (!$(e.target).closest('#burger').length) {
+      $('#mobile-menu').hide();
+      $('#burger').removeClass('burger-active');
+    }
+    e.stopPropagation();
+  });
+
+  // AOS init
+  AOS.init({
+    once: true
+  });
+
+});
