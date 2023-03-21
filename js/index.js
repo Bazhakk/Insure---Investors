f060dc888855b4da35e7773bfad045c482032586
unknown <bazhakk@gmail.com>
2023-03-21 21:06:59 +0200

    controle check css

--- js/index.js
+++ js/index.js
@@ -17,17 +17,4 @@ jQuery(document).ready(function ( $ ) {
   AOS.init({
     once: true
   });
-
-  //Mobile-menu
-  // $(document).ready(function(){
-  //           var $menu = $("#mobile-menu");
-  //           $(window).scroll(function(){
-  //               if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
-  //                   $menu.removeClass("default").addClass("fixed");
-  //               } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) 
-  //               {$menu.removeClass("fixed").addClass("default");
-  //   }});//scroll
-  //       });
-    
-
 });
