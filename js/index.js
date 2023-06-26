76cb8c621927edf2f6c15c0bb68d2ebd24eb8360
unknown <bazhakk@gmail.com>
2023-06-27 00:27:21 +0300

    creation and description of a new section

--- js/index.js
+++ js/index.js
@@ -305,8 +305,8 @@ jQuery(document).ready(function ( $ ) {
   //Leaders-slider
   $('.slider-leaders').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     arrows: false,
     dots: false
   });
